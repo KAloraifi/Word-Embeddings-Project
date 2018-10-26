1e82ba7a757b21318a512063b20c823146909277
Khaled Aloraifi <khalidaloraifi@gmail.com>
2018-10-26 19:27:05 +0300

    Imported a developed Word Embeddings using Word2Vector

--- main.py
+++ main.py
@@ -1,24 +1,109 @@
-from gensim.models import Word2Vec
+import gensim
+import logging
+import os
 
-# define training data
-FullCorpus = [['this', 'is', 'the', 'first', 'sentence', 'for', 'word2vec'],
-              ['this', 'is', 'the', 'second', 'sentence'],
-              ['yet', 'another', 'sentence'],
-              ['one', 'more', 'sentence'],
-              ['and', 'the', 'final', 'sentence']]
-FirstPartOfCorpus = FullCorpus[:len(FullCorpus) // 2]
-SecondPartofCorpus = FullCorpus[len(FullCorpus) // 2:]
-# train model
-model = Word2Vec(FullCorpus, min_count=1)
-# summarize the loaded model
-print(model)
-# summarize vocabulary
-words = list(model.wv.vocab)
-print(words)
-# access vector for one word
-print(model['sentence'])
-# save model
-model.save('model.bin')
-# load model
-new_model = Word2Vec.load('model.bin')
-print(new_model)
+logging.basicConfig(
+    format='%(asctime)s : %(levelname)s : %(message)s',
+    level=logging.INFO)
+
+
+def show_file_contents(input_file):
+    with open(input_file, 'rb') as f:
+        for i, line in enumerate(f):
+            print(line)
+            break
+
+
+def read_input(input_file):
+    """This method reads the input file which is in gzip format"""
+
+    logging.info("reading file {0}...this may take a while".format(input_file))
+    with open(input_file, 'rb') as f:
+        for i, line in enumerate(f):
+
+            if (i % 10000 == 0):
+                logging.info("read {0} reviews".format(i))
+            # do some pre-processing and return list of words for each review
+            # text
+            yield gensim.utils.simple_preprocess(line)
+
+
+if __name__ == '__main__':
+
+    abspath = os.path.dirname(os.path.abspath(__file__))
+    data_file = os.path.join(abspath, "../reviews_data.txt.gz")
+
+    # read the tokenized reviews into a list
+    # each review item becomes a serries of words
+    # so this becomes a list of lists
+    documents = list(read_input(data_file))
+    logging.info("Done reading data file")
+
+    # build vocabulary and train model
+    model = gensim.models.Word2Vec(
+        documents,
+        size=150,
+        window=10,
+        min_count=2,
+        workers=10)
+    model.train(documents, total_examples=len(documents), epochs=10)
+
+    # save only the word vectors
+    model.wv.save(os.path.join(abspath, "../vectors/default"))
+
+    w1 = "dirty"
+    print("Most similar to {0}".format(w1), model.wv.most_similar(positive=w1))
+
+    # look up top 6 words similar to 'polite'
+    w1 = ["polite"]
+    print(
+        "Most similar to {0}".format(w1),
+        model.wv.most_similar(
+            positive=w1,
+            topn=6))
+
+    # look up top 6 words similar to 'france'
+    w1 = ["france"]
+    print(
+        "Most similar to {0}".format(w1),
+        model.wv.most_similar(
+            positive=w1,
+            topn=6))
+
+    # look up top 6 words similar to 'shocked'
+    w1 = ["shocked"]
+    print(
+        "Most similar to {0}".format(w1),
+        model.wv.most_similar(
+            positive=w1,
+            topn=6))
+
+    # look up top 6 words similar to 'shocked'
+    w1 = ["beautiful"]
+    print(
+        "Most similar to {0}".format(w1),
+        model.wv.most_similar(
+            positive=w1,
+            topn=6))
+
+    # get everything related to stuff on the bed
+    w1 = ["bed", 'sheet', 'pillow']
+    w2 = ['couch']
+    print(
+        "Most similar to {0}".format(w1),
+        model.wv.most_similar(
+            positive=w1,
+            negative=w2,
+            topn=10))
+
+    # similarity between two different words
+    print("Similarity between 'dirty' and 'smelly'",
+          model.wv.similarity(w1="dirty", w2="smelly"))
+
+    # similarity between two identical words
+    print("Similarity between 'dirty' and 'dirty'",
+          model.wv.similarity(w1="dirty", w2="dirty"))
+
+    # similarity between two unrelated words
+    print("Similarity between 'dirty' and 'clean'",
+          model.wv.similarity(w1="dirty", w2="clean"))
